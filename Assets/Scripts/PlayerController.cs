217fc6601b7e0e2a2d6276dab22fc0bb1f422941
Koal M. Casler <Theeoblivion@gmail.com>
2024-09-25 13:00:13 -0300

    Got player controller "Working"

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,22 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private GameManager gameManager;
     private Transform spawnPoint;
     public Transform playerTransform;
     private Rigidbody playerBody;
     public bool hasLaunched;
     public bool isOffRamp;
     public int boostValue;
+    public float maxRotationX;
+    public float maxRotationY;
+    public float minRotationX;
+    public float minRotationY;
+
     
     void Awake()
     {
+        gameManager = GameManager.gameManager;
         playerBody = this.gameObject.GetComponent<Rigidbody>();
         playerTransform = this.transform;
     }
@@ -31,62 +38,125 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CheckRotation();
-        if(hasLaunched)
+        if(gameManager.gameState == GameManager.GameState.Gameplay)
         {
-
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = true;
+            if(isOffRamp)
+            {
+                CheckRotation();
+            }
+            if(!isOffRamp)
+            {
+                Quaternion rotation = new Quaternion();
+                rotation.Set(playerTransform.rotation.x,0,playerTransform.rotation.z,1);
+                playerTransform.rotation = rotation;
+            }
+            if(hasLaunched)
+            {
+                //Releases player from spawn position when launch it activated. 
+                playerBody.isKinematic = false;
+            }
+            else
+            {
+                //holds player in spawn position, until launch it activated. 
+                playerBody.isKinematic = true;
+            }
         }
         else
         {
             //holds player in spawn position, until launch it activated. 
-            playerTransform.position = spawnPoint.position;
-            playerTransform.rotation = spawnPoint.rotation;
+            playerBody.isKinematic = true;
+            Cursor.lockState = CursorLockMode.None;
         }
     }
 
     void OnLaunch()
     {
-        if(!hasLaunched)
+        if(gameManager.gameState == GameManager.GameState.Gameplay)
         {
-            hasLaunched = true;
+            if(!hasLaunched)
+            {
+                hasLaunched = true;
+            }
+        }
+    }
+    
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 moveVector = movementValue.Get<Vector2>();
+        if(gameManager.gameState == GameManager.GameState.Gameplay && isOffRamp)
+        {
+            RotatePlayer(moveVector);
         }
     }
 
     public void LaunchBoost()
     {
-        /// <summary>
-        /// Will add force to player based on upgrades the player has aquiered. 
-        /// </summary>
+        //Launched player at end of ramp based on upgrades
         isOffRamp = true;
         playerBody.AddForce(Vector3.forward * boostValue);
     }
 
+    public void RotatePlayer(Vector2 moveVector) // should only take in the move vector from OnMove Method
+    {
+        float step = playerBody.velocity.z * Time.deltaTime;
+        if(moveVector.x > 0)
+        {
+            Quaternion rotation = Quaternion.AngleAxis(30, transform.right);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, step*5);
+        }
+        if(moveVector.x < 0)
+        {
+            Quaternion rotation = Quaternion.AngleAxis(-30, transform.right);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, step*5);
+        }
+        if(moveVector.y > 0)
+        {
+            Quaternion rotation = Quaternion.AngleAxis(-25, transform.forward);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, step);
+        }
+        if(moveVector.y < 0)
+        {
+            Quaternion rotation = Quaternion.AngleAxis(25, transform.forward);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, step);
+        }
+        else
+        {
+            Quaternion rotation = new Quaternion();
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, step);
+        }
+    }
+
     void CheckRotation()
     {
-        if(playerTransform.rotation.x < -30)
+        if(playerTransform.rotation.x <= minRotationX)
         {
-            Debug.Log("Clamping View");
+            Debug.Log("Clamping rotation x =" + playerTransform.rotation.x);
             Quaternion rotation = Quaternion.AngleAxis(-30, transform.right);
+            rotation.Set(rotation.x,playerTransform.rotation.y,playerTransform.rotation.z,1);
             playerTransform.rotation = rotation;
         }
-        if(playerTransform.rotation.x > 30)
+        if(playerTransform.rotation.x >= maxRotationX)
         {
-            Debug.Log("Clamping View");
+            Debug.Log("Clamping rotation x =" + playerTransform.rotation.x );
             Quaternion rotation = Quaternion.AngleAxis(30, transform.right);
+            rotation.Set(rotation.x,playerTransform.rotation.y,playerTransform.rotation.z,1);
             playerTransform.rotation = rotation;
         }
-        if(playerTransform.rotation.z < -25)
+        if(playerTransform.rotation.z <= minRotationY)
         {
-            Debug.Log("Clamping View");
+            Debug.Log("Clamping rotation z =" + playerTransform.rotation.z);
             Quaternion rotation = Quaternion.AngleAxis(-25, transform.forward);
+            rotation.Set(playerTransform.rotation.x,playerTransform.rotation.y,rotation.z,1);
             playerTransform.rotation = rotation;
         }
-        if(playerTransform.rotation.z > 25)
+        if(playerTransform.rotation.z >= maxRotationY)
         {
-            Debug.Log("Clamping View");
+            Debug.Log("Clamping rotation z =" + playerTransform.rotation.z);
             Quaternion rotation = Quaternion.AngleAxis(25, transform.forward);
+            rotation.Set(playerTransform.rotation.x,playerTransform.rotation.y,rotation.z,1);
             playerTransform.rotation = rotation;
         }
-
     }
 }
