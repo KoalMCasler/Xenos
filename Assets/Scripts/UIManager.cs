d5fcfbfde6ef1ea8f4b10c4a796ad4d865e18011
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2024-10-01 17:21:04 -0300

    started work on HUD

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -39,6 +39,9 @@ public class UIManager : MonoBehaviour
     public TextMeshProUGUI moneyResultsText;
     public TextMeshProUGUI totalResultsText;
     public float runTotal;
+    [Header("HUD")]
+    public TextMeshProUGUI altitudeText;
+    public TextMeshProUGUI distanceText;
 
     void Awake()
     {
